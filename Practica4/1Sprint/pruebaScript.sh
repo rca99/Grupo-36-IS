f094915face7e494f592a3bce31b0d93e686574d
Fernando Herrera Poch <a52hepof@uco.es>
2018-12-04 17:58:28 +0100

    Fecha última copia seguridad: 4/12/2018

--- Practica4/1Sprint/pruebaScript.sh
+++ Practica4/1Sprint/pruebaScript.sh
@@ -13,36 +13,34 @@ echo "Numero de parametros": $#;
 echo "Todos los parametros menos el 0": $*;
 
 
-
-echo "Introduce la fecha de la Copia de seguridad:"
-read TXT;
-#git add gestionAlumnos.bin;
-read NOMBREARCHIVO=gestionAlumnos+'$TXT'.bin;
-mv  gestionAlumnos.bin  $NOMBREARCHIVO;
-
 git status;
+
+#git add gestionAlumnos.bin;
+
+
 cd ..
 git add .;
 clear;
-
-
+echo "Introduce la fecha de la Copia de seguridad:"
+read TXT;
 
 git commit -m "Fecha última copia seguridad: $TXT"
 
 git remote set-url origin https://a52hepof:vn9rn7rz@github.com/rca99/Grupo-36-IS.git
 
-#cd 1Sprint
-#git push -u origin master
-#rm gestionAlumnos.bin
+cd 1Sprint
+git push -u origin master
+rm gestionAlumnos.bin
 echo copia de seguridad realizada
 
+##descarga copia de seguridad externa
  /usr/bin/firefox -new-window https://github.com/rca99/Grupo-36-IS/raw/master/Practica4/1Sprint/gestionAlumnos.bin
 
 git remote set-url origin https://github.com/rca99/Grupo-36-IS.git
 
 
 find -iname "*ges*"
-
+##mv  Archivo1.ext Archivo2.ext
 
 
 ##https://www.google.com/search?safe=active&client=ubuntu&hs=XGb&channel=fs&ei=FawGXK2ZJezEgAaqzLLQDg&q=shell+script+for+git+pull&oq=script+shell+para+git&gs_l=psy-ab.3.0.0i22i30l6.10585.18010..19905...5.0..0.314.3158.2j18j1j1......0....1..gws-wiz.......0j35i39j0i67j0i131j0i131i67j0i20i263j0i10j0i131i20i263j0i203j0i22i10i30.FEAJGkGnw3M
