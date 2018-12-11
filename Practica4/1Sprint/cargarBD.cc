404f9888dba67bd6255df082d208d918e6faf8d1
Fernando Herrera Poch <a52hepof@uco.es>
2018-12-11 15:33:11 +0100

    actualización

--- Practica4/1Sprint/cargarBD.cc
+++ Practica4/1Sprint/cargarBD.cc
@@ -1,50 +1,143 @@
 
 #include "BD.h" //para incluir la lista de alumnos y funciones BD 
-//#include "Profesor.h"//para incluir puntero a base de datos
+#include "Profesor.h"//para incluir puntero a base de datos
 #include "Alumno.h"
 #include <list>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstring>
+#include "consoleLinux.h"//colores menu
+#include <iomanip>//formato cout
+
 
 using namespace std;
 
 
+//g++ -g -Wall  menuprincipal.cc guardarBD.cc cargarBD.cc Profesor.h Alumno.h consoleLinux.h BD.h Alumno.cc
+
+
+/*
+guardar BD()
+
+Descripción: Carga el contenido de la base de datos guardada en un fichero binario a la lista de alumnos
+
+Valores devueltos: 1 si ha cargado correctamente la base de datos y 0 si no existe el fichero binario que cargar
+
+Parámetros: no recibe ningún parámetro. Accede a listaAlumnos_ para guardarla
+
+//puntero a la dirección de memoria de la base de datos creo que no es necesario para la implementación
+
+*/
+
+
+
+
+
 bool BD::cargarBD(){
+	
 
+	Alumno aux; //para guardar los datos que se van leyendo. Su contenido es añadido a listaAlumnos_ a partir de la función introducirAlumno(alumno)
+
+	Profesor p;// para usar el puntero a la base de datos
+
+	//el método de leer fichero recibe el nombre del fichero como tipo char
+	char NombreFicheroBin[50];
+	string nameBD=getNombreFichero()+".bin";
+	strcpy(NombreFicheroBin, nameBD.c_str());
+	
+	
 	listaAlumnos_.clear();
+	cout<<listaAlumnos_.size()<<endl;
 
-	int registros=0;
-	int tamano;
-	list <Alumno>::iterator i; 
+	//******leer fichero*********//
+	FILE *ficheroLectura;
+	ficheroLectura=fopen(NombreFicheroBin, "rb");
+	
+	if (ficheroLectura==NULL){
+		return 0;
+	} //si existe el fichero lo sobreescribe
+
+	datosAlumno datoLectura;
+
+	p.setFicheroBD(datoLectura);
+
+	
+	cout<<"**************Datos Fichero******************************"<<endl;
+	cout<<"__________________________________________________________"<<endl;
+
+	//fseek(ficheroLectura,0L, SEEK_SET);
+	
+	fread(&datoLectura, sizeof(datosAlumno), 1, ficheroLectura);
+	//fread(p.getFicheroBD(), sizeof(datosAlumno), 1, ficheroLectura);//no funciona para el puntero hacia la base de datos definitdo en  Profesor.h
+	
+	while(!feof(ficheroLectura)){
+		cout<<datoLectura.nombre<<endl;
+		cout<<datoLectura.curso<<endl;
+				
+		aux.setNombre(datoLectura.nombre);
+		aux.setCurso(datoLectura.curso);
+
+		introducirAlumno(aux);
+		//fread(p.getFicheroBD(), sizeof(datosAlumno), 1, ficheroLectura);//no funciona para el puntero hacia la base de datos definitdo en  Profesor.h
+		fread(&datoLectura, sizeof(datosAlumno), 1, ficheroLectura);
+	}
 	
 
-	ifstream ifile(nombreFichero_, ios::in | ios::binary);
+	cout<<listaAlumnos_.size()<<endl;
+
+	list <Alumno>::iterator i;
+
+	system("clear");
 	
-	ifile.seekg(0L, ifile.end);
-	tamano=ifile.tellg();
+	cout<<BOLD_ON<<"\tLISTA DE ALUMNOS CARGADA EN EL SISTEMA________________"<<RESET<<endl;
+	cout<<""<<endl;
+	cout<<endl;
+	
+	cout<<fixed;
+	cout<<setprecision(2);
+	cout.fill('*');
 
-	registros=tamano/sizeof(Alumno);
-	cout<<"tamaño base datos"<<endl;
-	//cout<<"nº registros archivo binario:"<<registros<<endl;
+	cout<<left<<setw(40)<<"\t|Nombre|"<<
+		left<<setw(20)<<"\t|Curso|"<<endl;
+	
 
-	//ifile.read((char*)&listaAlumnos_,sizeof(Alumno)*registros);
+	for (i = listaAlumnos_.begin(); i !=listaAlumnos_.end(); ++i)
+	{
 
-	for (i=listaAlumnos_.begin(); i!=listaAlumnos_.end();i++){
-			//ofile.write((char*)p.ficheroBD_,sizeof(Alumno) );
-		cout<<"nombre:"<<i->getNombre()<<endl;
-			//cout<<"edad:"<<i->getEdad()<<endl;
-
+		if (i->getNombre()=="rodolfo"){
+			cout.fill('-');
+				
+			cout<<BOLD_ON<<COLOR_YELLOW<<left<<setw(40)<<setiosflags (ios::uppercase)<<i->getNombre()<<//setiosflags (std::ios::showbase | std::ios::uppercase)
+											left<<setw(20)<<i->getCurso()<<RESET<<endl;
+		}
+		else{
+			cout.fill(' ');
+			
+			cout<<COLOR_BRIGHTRED <<left<<setw(40)<<i->getNombre()<<
+											left<<setw(20)<<i->getCurso()<<COLOR_NORMAL<<endl;
+		
+		}
 
 	}
+	getchar();
+	getchar();
+	system("clear");
+	fclose(ficheroLectura);
 
-	//cout<<"Tamaño Base de Datos: "<<listaAlumnos_.size()<<endl;
-	ifile.close();
 
-		return 1;
-	
-	
 
-}
+	return 1;
+
+      
+  }
+
+
+
+
+
+
+
+
 
 
--- Practica4/1Sprint/guardarBD.cc
+++ Practica4/1Sprint/guardarBD.cc
@@ -4,56 +4,78 @@
 #include "BD.h" //para incluir la lista de alumnos y funciones BD 
 //#include "Profesor.h"//para incluir puntero a base de datos
 #include "Alumno.h"
-#include "Profesor.h"
 #include <list>
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstring>
+
 
 using namespace std;
 
+/*
+guardar BD()
+
+Descripción: Guarda el contenido de la lista de alumnos en un fichero binario
+
+Si no existe el fichero binario de la copia de seguridad, lo crea y si existe lo sobreescribe 
+con los datos de la lista de alumnos.
+
+Valores devueltos: 1 si ha guardado correctamente la base de datos.
+
+Parámetros: no recibe ningún parámetro. Accede a listaAlumnos_ para guardarla
+
+*/
+
 bool BD::guardarBD(){
 
-	/*Alumno a;
-	a.setNombre("Ramón");
-	//a.setEdad(65);
-	Alumno b;
-	b.setNombre("Fernando");
-	//b.setEdad(34);
-	Alumno c;
-	c.setNombre("Akela");
-	listaAlumnos_.push_back(a);
-	listaAlumnos_.push_back(b);
-	listaAlumnos_.push_back(c);
-	*/
-
-	Profesor p;
-	string nameBD=nombreFichero_+".bin";
-
-	list <Alumno>::iterator i; 
-
-	//int *ficheroBD;
-	//ficheroBD=(int*)&listaAlumnos_;
-	p.setFicheroBD(listaAlumnos_);
 	
-	ofstream ofile(nameBD, ios::binary);
+	//cout<<"tamaño lista: "<<listaAlumnos_.size()<<endl;
 
-	if (!ofile){
+	//el método de leer fichero recibe el nombre del fichero como tipo char
+	char NombreFicheroBin[50];
+	string nameBD=getNombreFichero()+".bin";
+	strcpy(NombreFicheroBin, nameBD.c_str());
 
-		return 0;
+
+
+	//*******Escribir Fichero **********//
+	FILE *fichero;
+	datosAlumno dato;
+
+	fichero=fopen(NombreFicheroBin, "wa+b");
+	if (fichero==NULL){
+		cout<<"no existe"<<endl;
+	} //si existe el fichero lo sobreescribe
+
+
+	list <Alumno>::iterator i;
+
+	list <Alumno> aux;
+	list <Alumno> auxiliarEscribir;
+
+	aux=listaAlumnos_;
+	
+	for (i = aux.begin(); i!=aux.end(); ++i)
+	{
+	
+		cout<<i->getNombre()<<"-"<<i->getCurso()<<"-"<<i->getApellidos()<<endl;
+		//dato.nombre=i->getNombre();
+
+		strcpy(dato.nombre, i->getNombre().c_str());
+		//strcpy(dato.apellidos, i->getApellidos().c_str());
+		dato.curso=i->getCurso();
+
+
+
+
+		fwrite(&dato, sizeof(datosAlumno), 1, fichero);
+
 	}
 
-	else{
-
-		for (i=listaAlumnos_.begin(); i!=listaAlumnos_.end();i++){
-			//ofile.write((char*)&listaAlumnos_,sizeof(Alumno));
-			ofile.write((char*)p.getFicheroBD(),sizeof(Alumno));
-
-		}
-
-		ofile.close();
-		return 1;
-	}
+	fclose(fichero);
+	return 1;
 
 
 }
+
