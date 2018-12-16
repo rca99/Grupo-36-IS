aca41e8026e6ce1b9817522a9f52e011a30e8d56
Fernando Herrera Poch <a52hepof@uco.es>
2018-12-16 18:30:54 +0100

    pruebas externa

--- Practica4/2Sprint/AppGestionAlumnos/guardarExterna.sh
+++ Practica4/2Sprint/AppGestionAlumnos/guardarExterna.sh
@@ -15,7 +15,7 @@ git commit -m "Fecha última copia seguridad: $TXT"
 
 git remote set-url origin https://a52hepof:vn9rn7rz@github.com/rca99/Grupo-36-IS.git
 
-cd ..
+
 git push -u origin master
 #rm gestionAlumnos.bin
 echo -e "\e[34mCOPIA DE SEGURIDAD REALIZADA\e[0m"
