949a085d049eaac66128da57b4f1fcd29d3619b5
Fernando Herrera Poch <a52hepof@uco.es>
2018-12-16 18:39:11 +0100

    externa ok

--- Practica4/2Sprint/AppGestionAlumnos/cargarExterna.sh
+++ Practica4/2Sprint/AppGestionAlumnos/cargarExterna.sh
@@ -7,7 +7,7 @@ sleep 2;
 
 git remote set-url origin https://a52hepof:vn9rn7rz@github.com/rca99/Grupo-36-IS.git
 
-wget -N -P ../AppGestionAlumnos/CopiaSeguridad https://github.com/rca99/Grupo-36-IS/raw/master/Practica4/2Sprint/Fernando/AppGestionAlumnos/gestionAlumnos.bin
+wget -N -P ../AppGestionAlumnos/CopiaSeguridad https://github.com/rca99/Grupo-36-IS/raw/master/Practica4/2Sprint/AppGestionAlumnos/gestionAlumnos.bin
 
 git remote set-url origin https://github.com/rca99/Grupo-36-IS.git
 
